2af08fe188311032d33679b035cbc0ebe87fad7d
Fabio Colacio <fabio.colacio.dev@gmail.com>
2020-02-08 17:34:54 -0800

    Update migration to create all tables

--- priv/repo/migrations/20191103185640_create_tables.exs
+++ priv/repo/migrations/20191103185640_create_tables.exs
@@ -10,12 +10,38 @@ defmodule Bast.Controller.Repo.Migrations.CreateTables do
     create table(:users) do
       add :name,    :string, size: 32, null: false
       add :email,   :string
-      add :phoneno, :string, size: 10
-      add :pubkey,  :binary
       add :pin,     :string
       add :cardno,  :integer
       add :lastaccess, references(:doors)
     end
     unique_index(:users, [:email, :phoneno, :cardno])
+
+    create table(:roles) do
+      add :name, :string, null: false
+    end
+
+    create table(:userrole) do
+      add :user, references(:roles), null: false
+      add :role, references(:users), null: false
+    end
+
+    create table(:activetimes) do
+      add :start, :utc_datetime, null: false
+      add :end,   :utc_datetime, null: false
+    end
+
+    create table(:permissions) do
+      add :role, references(:roles), null: false
+    end
+
+    create table(:credentialtypes) do
+      add :permission, references(:permissions), null: false
+      add :type,       :integer,                 null: false
+    end
+
+    create table(:permissiontarget) do
+      add :door, references(:doors),             null: false
+      add :permission, references(:permissions), null: false
+    end
   end
 end
